249f5e7aa1626aa2a00d57745db946083ad906fd
blackice18 <sdsdfhshs.dima@yandex.ru>
2022-02-03 20:01:09 +0300

    добавелние php

--- js/Requests.js
+++ js/Requests.js
@@ -5,14 +5,26 @@ $(document).ready(function(){
 });
 
 function SendedRequests() {
+   /*
     axios({
         method: 'get',
         url: API_URL+'requests',
         responseType: 'stream',
         headers: {'X-Requested-With': 'XMLHttpRequest'},
+        timeout: 50000
     })
     .then(function (response) {
         //response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+        console.log(response);
+    });
+    */
+    var settings = {
+        "url": "http://164.92.215.12:8644/requests",
+        "method": "GET",
+        "timeout": 0,
+    };
+
+    $.ajax(settings).done(function (response) {
         console.log(response);
     });
 }
